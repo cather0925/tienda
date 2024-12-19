79c6f9429b4932843204729f9e15d26994f9b4a4
Rosa catherine Torres ortega <rosacatherinet@gmail.com>
2024-12-18 21:10:09 -0500

    crear las clases model, mapeo clases a tableas, integarcion vistas

--- src/main/java/com/tienda/dona/rosa/model/Usuario.java
+++ src/main/java/com/tienda/dona/rosa/model/Usuario.java
@@ -1,5 +1,76 @@
 package com.tienda.dona.rosa.model;
 
+import java.util.List;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "usuarios")
 public class Usuario {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer id;
+	private String nombre;
+	private String username;
+	private String password;
+	
+	@OneToMany (mappedBy = "usuario")
+	private List<Producto> productos;
+	
+	public Usuario (){	
+	}
+	
+	public Usuario(Integer id, String nombre, String username, String password) {
+		super();
+		this.id = id;
+		this.nombre = nombre;
+		this.username = username;
+		this.password = password;
+	}
+	public Integer getId() {
+		return id;
+	}
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	public String getUsername() {
+		return username;
+	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	
+	
+
+	public List<Producto> getProductos() {
+		return productos;
+	}
+
+	public void setProductos(List<Producto> productos) {
+		this.productos = productos;
+	}
+
+	@Override
+	public String toString() {
+		return "Usuario [id=" + id + ", nombre=" + nombre + ", username=" + username + ", password=" + password + "]";
+	}
+	
 
 }
